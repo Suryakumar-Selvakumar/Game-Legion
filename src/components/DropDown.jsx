d687990bd3d46e06f8ebd24e055647a6bfe074b3
Suryakumar Selvakumar <ssuryakumar2077@gmail.com>
2025-01-04 16:59:37 -0700

    feat(`DropDown.jsx`): + component design & logic

--- src/components/DropDown.jsx
+++ src/components/DropDown.jsx
@@ -10,21 +10,31 @@ const StyledDropDown = styled.div`
   padding: 1.5rem 2rem 0rem 1rem;
   position: relative;
   margin-left: 1rem;
+  width: max-content;
 `;
 
 const MenuOpener = styled.div`
   background-color: white;
-  width: 225px;
+  width: 220px;
   display: flex;
-  padding: 5px 7.5px;
+  padding: 5px 10px;
   border-radius: 10px;
-  justify-content: center;
+  justify-content: space-between;
   gap: 0.25rem;
   cursor: pointer;
   align-items: center;
 
   svg {
     width: 25px;
+    transition: 250ms cubic-bezier(0.61, -0.53, 0.43, 1.43);
+  }
+
+  svg.open {
+    transform: rotate(180deg);
+  }
+
+  svg.close {
+    transform: rotate(0deg);
   }
 `;
 
@@ -32,10 +42,6 @@ const growDown = keyframes`
     0%{
         transform: scaleY(0);
     }
-
-    /* 80% {
-        transform: scaleY(1.1);
-    } */
 
     100% {
         transform: scaleY(1);
@@ -45,10 +51,12 @@ const growDown = keyframes`
 const growUp = keyframes`
     0%{
         transform: scaleY(1);
+        opacity: 1;
     }
 
     100% {
         transform: scaleY(0);
+        opacity: 0;
     }
 `;
 
@@ -57,15 +65,17 @@ const Menu = styled.ul`
   list-style-type: none;
   padding: 4.5px 7.5px;
   border: 1px solid grey;
-  width: 225px;
+  width: 220px;
   background-color: white;
   border-radius: 10px;
   z-index: 3;
   cursor: pointer;
   transform-origin: top center;
+  transform: scaleY(0);
 
   &.open {
     animation: ${growDown} 250ms cubic-bezier(0.61, -0.53, 0.43, 1.43) forwards;
+    box-shadow: 0px 4px 10px rgb(0, 0, 0, 0.25);
   }
 
   &.close {
@@ -86,7 +96,7 @@ const MenuItem = styled.li`
   justify-content: space-between;
   align-items: center;
   border-radius: 10px;
-  transition: all 500ms ease;
+  transition: all 250ms ease;
 
   &:hover {
     background-color: #e3e3e3;
@@ -103,9 +113,11 @@ class DropDown extends Component {
 
     this.state = {
       dropDownOpen: false,
+      firstLoad: true,
     };
 
     this.setDropDownOpen = this.setDropDownOpen.bind(this);
+    this.setFirstLoad = this.setFirstLoad.bind(this);
     this.dropDownRef = createRef(null);
   }
 
@@ -113,6 +125,13 @@ class DropDown extends Component {
     this.setState((state) => ({
       ...state,
       dropDownOpen: !this.state.dropDownOpen,
+    }));
+  }
+
+  setFirstLoad() {
+    this.setState((state) => ({
+      ...state,
+      firstLoad: false,
     }));
   }
 
@@ -138,24 +157,47 @@ class DropDown extends Component {
   render() {
     return (
       <StyledDropDown ref={this.dropDownRef}>
-        <MenuOpener onClick={this.setDropDownOpen}>
+        <MenuOpener
+          onClick={() => {
+            this.setDropDownOpen();
+            this.setFirstLoad();
+          }}
+        >
           <span>
             Order By: <b>{this.props.orderBy}</b>
           </span>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <svg
+            className={
+              this.state.firstLoad
+                ? ""
+                : this.state.dropDownOpen
+                ? "open"
+                : "close"
+            }
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
             <path
-              fill="rgb(67, 67, 67)"
+              fill="rgb(45, 45, 45)"
               d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"
             />
           </svg>
         </MenuOpener>
 
-        <Menu className={this.state.dropDownOpen ? "open" : "close"}>
+        <Menu
+          className={
+            this.state.firstLoad
+              ? ""
+              : this.state.dropDownOpen
+              ? "open"
+              : "close"
+          }
+        >
           <MenuItem
             onClick={() => {
               this.props.setOrderBy("Name");
+              this.setDropDownOpen();
             }}
-            onAnimationEnd={this.setDropDownOpen}
           >
             <span>Name</span>
             {this.props.orderBy === "Name" && (
@@ -165,8 +207,8 @@ class DropDown extends Component {
           <MenuItem
             onClick={() => {
               this.props.setOrderBy("Release Date");
+              this.setDropDownOpen();
             }}
-            onAnimationEnd={this.setDropDownOpen}
           >
             <span>Release Date</span>
             {this.props.orderBy === "Release Date" && (
@@ -176,8 +218,8 @@ class DropDown extends Component {
           <MenuItem
             onClick={() => {
               this.props.setOrderBy("Popularity");
+              this.setDropDownOpen();
             }}
-            onAnimationEnd={this.setDropDownOpen}
           >
             <span>Popularity</span>
             {this.props.orderBy === "Popularity" && (
@@ -187,8 +229,8 @@ class DropDown extends Component {
           <MenuItem
             onClick={() => {
               this.props.setOrderBy("Rating");
+              this.setDropDownOpen();
             }}
-            onAnimationEnd={this.setDropDownOpen}
           >
             <span>Rating</span>
             {this.props.orderBy === "Rating" && (
