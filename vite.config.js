57412c0f91f69cbab437b0f96fee616f6077f2a9
Suryakumar Selvakumar <ssuryakumar2077@gmail.com>
2025-01-04 21:10:16 -0700

    fix: Change `vite.config.js` and delete `.babelrc`

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,21 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          [
+            "babel-plugin-styled-components",
+            {
+              displayName: true,
+              fileName: false,
+            },
+          ],
+        ],
+      },
+    }),
+  ],
   test: {
     globals: true,
     environment: "jsdom",
