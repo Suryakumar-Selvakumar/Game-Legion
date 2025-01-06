0a2f1daea11a6e7c40d3d69feed4d0f524419203
Suryakumar Selvakumar <ssuryakumar2077@gmail.com>
2025-01-05 17:04:00 -0700

    refactor(`Loading.jsx`): Use `react-cssfx-loading`
    Switch to react-cssfx-loading library for a smoother loading animation

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -82,6 +82,10 @@ const GameName = styled.p`
 class GameCard extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      loading: true,
+    };
   }
 
   render() {
--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,34 +1,6 @@
 import { Component } from "react";
-import styled, { keyframes, ThemeContext } from "styled-components";
-import PropTypes from "prop-types";
-
-const animate = keyframes`
-     0% {
-        stroke-dashoffset: 270;
-     }
-
-     50% {
-        stroke-dashoffset: 0;
-     }
-
-     50.1% {
-        stroke-dashoffset: 540;
-     }
-
-     100% {
-        stroke-dashoffset: 270;
-     }
-`;
-
-const rotate = keyframes`
-0% {
-    transform: rotate(0deg)
-}
-
-100% {
-    transform: rotate(360deg);
-}
-`;
+import styled, { ThemeContext } from "styled-components";
+import { CircularProgress } from "react-cssfx-loading";
 
 const StyledLoading = styled.div`
   display: flex;
@@ -36,25 +8,6 @@ const StyledLoading = styled.div`
   align-items: start;
   min-height: 100vh;
   padding-top: 7.5rem;
-
-  svg {
-    position: relative;
-    width: ${(props) => props.size}px;
-    height: ${(props) => props.size}px;
-    animation: ${rotate} 1.5s linear infinite;
-  }
-
-  svg circle {
-    fill: none;
-    stroke-width: 5;
-    stroke: ${(props) =>
-      props.theme.currentTheme === "norse" ? "#46afe8" : "#ff5a5a"};
-    stroke-linecap: round;
-    transform: translate(5px, 5px);
-    stroke-dasharray: ${(props) => 2 * Math.PI * (props.size / 2)};
-    stroke-dashoffset: ${(props) => 2 * Math.PI * (props.size / 2)};
-    animation: ${animate} 3s linear infinite;
-  }
 `;
 
 class Loading extends Component {
@@ -62,12 +15,19 @@ class Loading extends Component {
     super(props);
   }
 
+  static contextType = ThemeContext;
+
   render() {
+    const theme = this.context;
+
     return (
-      <StyledLoading size={90}>
-        <svg>
-          <circle cx="40" cy="40" r="40"></circle>
-        </svg>
+      <StyledLoading>
+        <CircularProgress
+          color={theme.currentTheme === "norse" ? "#46afe8" : "#ff5a5a"}
+          duration="2s"
+          width="100px"
+          height="100px"
+        />
       </StyledLoading>
     );
   }
